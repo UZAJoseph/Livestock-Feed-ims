ca9fbd7c2c00151196ca506d1699bdfd44e245d4
joseph <uzajoseph@gmail.com>
2026-07-28 16:44:13 +0300

    Add Report and Filter

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
      path('feeds/', views.feed_selector_page, name='feed_selector_page'),
+     path('reports/', views.reports_dashboard, name='reports_dashboard'),
     path('feeds/animal/<int:animal_id>/feed-types/', views.get_feed_types, name='get_feed_types'),
     path('feeds/feed-type/<int:feedtype_id>/description/', views.get_feed_description, name='get_feed_description'),
     path("order/", views.order_form, name="order_form"),
