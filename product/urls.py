e4df8f568bb324baf2b65646fb0c7f2d03ff228e
joseph <uzajoseph@gmail.com>
2026-08-02 17:02:46 +0300

    add some style on confirmation notification

--- product/urls.py
+++ product/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = [
     path('study/', views.study, name='study'),
-    path('index/', views.index, name='index'),
+    path('index/', views.home, name='home'),
      path('feeds/', views.feed_selector_page, name='feed_selector_page'),
      path('reports/', views.reports_dashboard, name='reports_dashboard'),
     path('feeds/animal/<int:animal_id>/feed-types/', views.get_feed_types, name='get_feed_types'),
     path('feeds/feed-type/<int:feedtype_id>/description/', views.get_feed_description, name='get_feed_description'),
     path("order/", views.order_form, name="order_form"),
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('animals/', views.animal_list, name ='animal_list')
 ]
 
